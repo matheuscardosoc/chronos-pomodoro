d312684d710f1d91f4c5a5242074bf913112edbb
Matheus Cardoso <matheuscardosoc@outlook.com>
2025-07-20 13:29:44 -0300

    Adicionando novo component Logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import './styles/theme.css';
-import './styles/global.css';
-
 import { Container } from './components/Container';
 import { Heading } from './components/Heading';
+import { Logo } from './components/Logo';
+
+import './styles/theme.css';
+import './styles/global.css';
 
 export function App() {
   return (
     <>
       <Container>
-        <Heading>LOGO</Heading>
+        <Logo />
       </Container>
+
       <Container>
         <Heading>Menu</Heading>
       </Container>
---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,15 @@
+import { Timer } from 'lucide-react';
+import styles from './styles.module.css';
+
+export function Logo() {
+  return (
+    <>
+      <div className={styles.logo}>
+        <a href='/' className={styles.logoLink}>
+          <Timer className={styles.svg} />
+          <span>Chronos</span>
+        </a>
+      </div>
+    </>
+  );
+}
