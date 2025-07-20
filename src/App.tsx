955f975db74765929c715d32f0cbeafcda588a54
Matheus Cardoso <matheuscardosoc@outlook.com>
2025-07-20 20:35:40 -0300

    Adicionando component CountDown

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Logo } from './components/Logo';
 import './styles/theme.css';
 import './styles/global.css';
 import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
 
 export function App() {
   return (
@@ -15,6 +16,10 @@ export function App() {
 
       <Container>
         <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
       </Container>
     </>
   );
