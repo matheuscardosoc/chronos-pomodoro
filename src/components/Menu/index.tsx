92acb9c7af20d1ea67cd279239530477c2e242e8
Matheus Cardoso <matheuscardosoc@outlook.com>
2025-07-24 20:12:44 -0300

    Adicionando theme no localStorage do navegador, e alterando icons na troca do tema

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,46 +1,39 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
+import {
+  HistoryIcon,
+  HouseIcon,
+  MoonIcon,
+  SettingsIcon,
+  SunIcon,
+} from 'lucide-react';
 import styles from './styles.module.css';
 import { useState, useEffect } from 'react';
 
 type AvailableThemes = 'dark' | 'light';
 
 export function Menu() {
-  const [theme, setTheme] = useState<AvailableThemes>('dark');
+  const [theme, setTheme] = useState<AvailableThemes>(() => {
+    const storageTheme =
+      (localStorage.getItem('theme') as AvailableThemes) || 'dark';
+
+    return storageTheme;
+  });
+
+  const nextThemeIcon = {
+    dark: <SunIcon />,
+    light: <MoonIcon />,
+  };
 
   function handleThemeChange(
     event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
   ) {
     event.preventDefault();
-
     setTheme(prevTheme => (prevTheme === 'dark' ? 'light' : 'dark'));
-
-    // setTheme(prevTheme => {
-    //   const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
-    //   return nextTheme;
-    // });
   }
 
-  // -- Executa toda vez que o componente renderiza na tela ------
-  // useEffect(() => {
-  //   document.documentElement.setAttribute('data-theme', theme);
-  // });
-  // -------------------------------------------------------------
-
-  // -- Executa apenas quando o React monta o componente na tela pela primeira vez --
-  // useEffect(() => {
-  //   console.log('useEffect de dependencias vazio', Date.now());
-  // }, []);
-  // --------------------------------------------------------------------------------
-
-  // -- Executa apenas quando o valor da dependência mudar --
   useEffect(() => {
     document.documentElement.setAttribute('data-theme', theme);
-
-    // return () => {
-    //   console.log('cleanup');
-    // };
+    localStorage.setItem('theme', theme);
   }, [theme]);
-  // ---------------------------------------------------------
 
   return (
     <nav className={styles.menu}>
@@ -75,7 +68,7 @@ export function Menu() {
         href='#'
         onClick={handleThemeChange}
       >
-        <SunIcon />
+        {nextThemeIcon[theme]}
       </a>
     </nav>
   );
