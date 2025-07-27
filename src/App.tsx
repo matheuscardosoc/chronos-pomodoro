6a199cb32dd55dddde3282265e00853e88a8b2e6
Matheus Cardoso <matheuscardosoc@outlook.com>
2025-07-26 22:37:29 -0300

    Ajustadas referências do App.Tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import { Home } from './pages/Home';
 
 import './styles/theme.css';
 import './styles/global.css';
-import { AboutPomodoro } from './pages/AboutPomodoro';
 
 export function App() {
-  return <AboutPomodoro />;
+  return <Home />;
 }
