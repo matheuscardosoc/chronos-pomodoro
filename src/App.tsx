d1d34d5ff118872f77e60fb6a0c0577b9f309012
Matheus Cardoso <matheuscardosoc@outlook.com>
2025-07-26 22:37:02 -0300

    Adicionado componente GenericHtml, pages NotFound e AboutPomodoro

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { Home } from './pages/Home';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { AboutPomodoro } from './pages/AboutPomodoro';
 
 export function App() {
-  return <Home />;
+  return <AboutPomodoro />;
 }
